d6815bed578fa14f40e50562363d04f075862d64
faithmwamkinga <faithmwamkinga@gmail.com>
2023-07-06 09:21:03 +0300

    variables

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,42 @@
+fun main() {
+   var name = "sheila"
+    println(name)
+    var classroom = "Adalab"
+    classroom = "AnitaB"
+
+    var wheels: Byte = 4
+    var nairobiPop: Short = 2198
+    var worldPop: Long = 46747875644
+    var  myAge: Int = 24
+    var iAmTwo: Boolean = false
+    var iAmAfrican: Boolean = true
+
+
+
+
+
+
+
+
+
+
+    // Characters
+    var initial = 'h'
+
+    //String
+    var group = "hopperlab"
+
+    //Booleans
+    var sunIsOut: Boolean = false
+    var sitting: Boolean = true
+
+    //Array
+    var friends = arrayOf("james","mina","ally")
+    var friendsAges = arrayOf(24,34,50)
+
+
+
+
+
+
+}
